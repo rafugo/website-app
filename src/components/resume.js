784f69c723f7a2949f9ec84c19ac57e8d39b9c9a
Rafael Fueyo-Gomez <rfueyogo@caltech.edu>
2020-05-19 11:21:19 -0700

    adding the logo.svg and also correcting the path to the resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -25,7 +25,7 @@ class Resume extends Component {
       <div className='resume-color'>
         <div className='resume-pdf-viewer'>
           <Document
-            file="/resume_april_1_2020.pdf"
+            file="website-app/resume_april_1_2020.pdf"
             onLoadSuccess={this.onDocumentLoadSuccess}
             onLoadError={console.error}
           >
